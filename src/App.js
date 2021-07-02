3a92991d8de127d54a8dc5fe2b0bbdd017902ea5
Sajjad Ahmad <jogimar14@gmail.com>
2021-07-02 19:48:14 +0500

    Fix eslint and style errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
+const App = () => (
+  <div className="App">
     <h2>This is app component!</h2>
-    </div>
-  );
-}
+  </div>
+);
 
 export default App;
