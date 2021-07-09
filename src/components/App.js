596409d8741e57d299398b1486ecc34f633d3221
Sajjad Ahmad <jogimar14@gmail.com>
2021-07-09 19:28:21 +0500

    Add a books table component and style it

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Book from './Book';
+import BookList from './BookList';
+import '../App.css';
+
+const App = () => (
+  <div className="App">
+    <Book />
+    <BookList />
+  </div>
+);
+
+export default App;
---
+++ src/components/Book.js
@@ -0,0 +1,5 @@
+const Book = () => (
+  <div>This is book component</div>
+);
+
+export default Book;
