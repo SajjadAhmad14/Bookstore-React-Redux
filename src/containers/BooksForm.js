d5780f8ba145446b0b8c047ccbe6c64fb9f1f63e
Sajjad Ahmad <jogimar14@gmail.com>
2021-07-28 15:56:47 +0500

    Add styling to form

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import 'semantic-ui-css/semantic.min.css';
 import { useDispatch } from 'react-redux';
 import allActions from '../store/actions/index';
 
@@ -41,27 +42,32 @@ const BooksForm = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="title"
-          id="title"
-          onChange={handleInputChange}
-        />
-        <select
-          name="categories"
-          id="categories"
-          onChange={handleCategoryChange}
-        >
-          <option value={CATEGORIES[0]}>{CATEGORIES[0]}</option>
-          <option value={CATEGORIES[1]}>{CATEGORIES[1]}</option>
-          <option value={CATEGORIES[2]}>{CATEGORIES[2]}</option>
-          <option value={CATEGORIES[3]}>{CATEGORIES[3]}</option>
-          <option value={CATEGORIES[4]}>{CATEGORIES[4]}</option>
-          <option value={CATEGORIES[5]}>{CATEGORIES[5]}</option>
-          <option value={CATEGORIES[6]}>{CATEGORIES[6]}</option>
-        </select>
-        <button type="submit">Add Book</button>
+      <form className="ui form" onSubmit={handleSubmit}>
+        <h4 className="ui dividing header">BOOK STORE</h4>
+        <div className="field">
+          <label htmlFor="title">
+            Book Title
+            <div className="two fields">
+              <div className="field">
+                <input type="text" name="title" id="title" onChange={handleInputChange} placeholder="First Name" />
+              </div>
+            </div>
+          </label>
+        </div>
+        <div className="two fields">
+          <div className="field">
+            <select className="ui fluid dropdown" name="categories" id="categories" onChange={handleCategoryChange}>
+              <option value={CATEGORIES[0]}>{CATEGORIES[0]}</option>
+              <option value={CATEGORIES[1]}>{CATEGORIES[1]}</option>
+              <option value={CATEGORIES[2]}>{CATEGORIES[2]}</option>
+              <option value={CATEGORIES[3]}>{CATEGORIES[3]}</option>
+              <option value={CATEGORIES[4]}>{CATEGORIES[4]}</option>
+              <option value={CATEGORIES[5]}>{CATEGORIES[5]}</option>
+              <option value={CATEGORIES[6]}>{CATEGORIES[6]}</option>
+            </select>
+          </div>
+        </div>
+        <button type="submit" className="ui button">ADD BOOK</button>
       </form>
     </div>
   );
